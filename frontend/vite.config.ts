a9e4ef185af40c3e2a94b994e84c3b0b6f800427
maliex4mix4 <malikopeyemi5@gmail.com>
2025-12-19 00:48:51 +0100

    chore: vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://api.studio.0xy7d.xyz',
+        target: 'http://localhost:8000',
         changeOrigin: true
       }
     }
